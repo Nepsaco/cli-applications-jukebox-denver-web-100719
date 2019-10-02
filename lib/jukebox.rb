8e84db4a5a91c58e405ef8e4f0538e56a3a4218f
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-10-02 21:09:46 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,7 +26,7 @@ def play(songs)
       #binding.pry
     if songs.include?(user_input) 
       puts "Playing #{user_input}"
-    elsif (1..9).to_a user_input.to_i >= 1  
+    elsif (1..9).to_a.include? user_input.to_i 
       puts "Playing #{song_name[user_input.to_i - 1]}"
     else
       puts "Invalid input, please try again"
